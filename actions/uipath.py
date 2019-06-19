6982c1f3ab43d4d84c259a56c79d0b7616e73ead
Álvaro Arcos <arcos1989@gmail.com>
2019-06-19 13:01:18 +0200

    Improve UiPathLib logging messages and do not verify SSL on requests

--- actions/uipath.py
+++ actions/uipath.py
@@ -28,6 +28,7 @@ class UiPathAPI:
         if not self.token: self.authenticate()
 
     def authenticate(self):
+        logger.info('Calling authenticate method...')
         url = self.host + '/api/Account/Authenticate'
         payload = {
             'tenancyName': os.getenv('TENANCY_NAME'),
@@ -35,32 +36,39 @@ class UiPathAPI:
             'password': os.getenv('PASSWORD')
         }
 
-        response = requests.request("POST", url, data=json.dumps(payload), headers=self.headers).json()
+        response = requests.request("POST", url, data=json.dumps(payload), headers=self.headers, verify=False).json()
         if response.get('success'):
-            # logger.info('User authenticated')
+            logger.info('User authenticated')
             self.token = response.get('result')
             self.headers['Authorization'] = 'Bearer {}'.format(self.token)
 
     def get_all_assets(self):
+        logger.info('Calling get_all_assets method...')
         url = self.host + '/odata/Assets'
-        response = requests.request("GET", url, headers=self.headers).json()
+        response = requests.request("GET", url, headers=self.headers, verify=False).json()
+        logger.info('Returning assets')
         return response.get('value')
 
     def get_all_robots(self):
+        logger.info('Calling get_all_robots method...')
         url = self.host + '/odata/Sessions'
         payload = {
             '$select': 'State',
             '$expand': 'Robot'
         }
-        response = requests.request("GET", url, params=payload, headers=self.headers).json()
+        response = requests.request("GET", url, params=payload, headers=self.headers, verify=False).json()
+        logger.info('Returning robots')
         return response.get('value')
 
     def get_all_sessions(self):
+        logger.info('Calling get_all_sessions method...')
         url = self.host + '/odata/Sessions'
-        response = requests.request("GET", url, headers=self.headers).json()
+        response = requests.request("GET", url, headers=self.headers, verify=False).json()
+        logger.info('Returning sessions')
         return response.get('value')
 
     def get_all_jobs(self, start_time_from=None, start_time_to=None, end_time_from=None, end_time_to=None, status=None):
+        logger.info('Calling get_all_jobs method...')
         url = self.host + '/odata/Jobs'
         filter_clauses = []
         if start_time_from: filter_clauses.append('StartTime gt {}'.format(start_time_from))
@@ -72,10 +80,7 @@ class UiPathAPI:
         if filter_clauses:
             filter_clause = ' and '.join(filter_clauses)
             payload['$filter'] = filter_clause
-            print(filter_clause)
-        response = requests.request("GET", url, params=payload, headers=self.headers).json()
+            logger.info('[get_all_jobs] Filter clause = {}'.format(filter_clause))
+        response = requests.request("GET", url, params=payload, headers=self.headers, verify=False).json()
+        logger.info('Returning jobs')
         return response.get('value')
-
-
-# api = UiPathAPI()
-# print('Number of assets: {}'.format(api.get_all_assets().get('@odata.count')))
